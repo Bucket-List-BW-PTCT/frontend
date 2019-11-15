830bf4766df83fc2923ec359f10e742771ac6024
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2019-11-14 21:16:26 -0500

    :sparkles: nav
    navbar started

--- bucket-list/src/App.js
+++ bucket-list/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       {/* <Signin /> */}
       <Signup />
     </div>
