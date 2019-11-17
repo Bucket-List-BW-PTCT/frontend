d4f24d599fe3f016c3f0b60cf6c7ff19bc64fa79
Gaetano Daidone <gaetano@daidone.dev>
2019-11-17 17:43:30 -0500

    :art: Fixed Signin
    Submit handle was not working properly, edited the code which now submits with formik. The API needs to be corrected. #1

--- bucket-list/src/components/Signin.js
+++ bucket-list/src/components/Signin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import styled from "styled-components";
 import * as Yup from "yup";
@@ -31,61 +31,131 @@ const HeaderStyle = styled.h2`
   color: grey;
 `;
 
-function SignIn(touched, errors) {
-  return (
-    <div>
-      <DivStyle>
-        <HeaderStyle>Sign In</HeaderStyle>
-        <Form>
-          <FormDiv>
-            {touched.username && errors.username && <p>{errors.username}</p>}
-            <Field
-              className="userInfo"
-              type="text"
-              name="username"
-              placeholder="Username or email"
-            />
-            {touched.password && errors.password && <p>{errors.password}</p>}
-            <Field
-              className="userInfo"
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-            <Button type='submit' color="success">Sign In</Button>
+// function SignIn(touched, errors) {
+//   return (
+//     <div>
+//       <DivStyle>
+//         <HeaderStyle>Sign In</HeaderStyle>
+//         <Form>
+//           <FormDiv>
+//             {touched.username && errors.username && <p>{errors.username}</p>}
+//             <Field
+//               className="userInfo"
+//               type="text"
+//               name="username"
+//               placeholder="Username or email"
+//             />
+//             {touched.password && errors.password && <p>{errors.password}</p>}
+//             <Field
+//               className="userInfo"
+//               type="password"
+//               name="password"
+//               placeholder="Password"
+//             />
+//             <Button type='submit' color="success">Sign In</Button>
 
-          </FormDiv>
+//           </FormDiv>
+//         </Form>
+//       </DivStyle>
+//       <ParaStyle>BUCKETLIST - 2019</ParaStyle>
+//     </div>
+//   );
+// }
+const Login = ({
+  touched,
+  errors,
+  status,
+  setToken,
+  setWelcomeMessage,
+  setUserID
+}) => {
+  useEffect(() => {
+    if (status) {
+      setToken(status.token);
+      setWelcomeMessage(status.message);
+      setUserID(status.userID);
+    }
+  }, [status]);
+
+  return (
+    <div textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <DivStyle>
+        <Form>
+  
+            <div className="ui fluid input">
+              <Field type="username" name="username" placeholder="Username" />
+            </div>
+            {touched.username && errors.username && (
+              <p className="error">{errors.username}</p>
+            )}
+
+            <div className="ui fluid input">
+              <Field type="password" name="password" placeholder="Password" />
+            </div>
+            {touched.password && errors.password && (
+              <p className="error">{errors.password}</p>
+            )}
+
+            <Button type="submit" fluid>
+              Login
+            </Button>
+
         </Form>
-      </DivStyle>
-      <ParaStyle>BUCKETLIST - 2019</ParaStyle>
+        </DivStyle>
     </div>
   );
-}
+};
 
-const SignInFormikForm = withFormik({
-  mapPropsToValues({ firstName, lastName, username, password }) {
+
+// const SignInFormikForm = withFormik({
+//   mapPropsToValues({ firstName, lastName, username, password }) {
+//     return {
+//       firstName: firstName || "",
+//       lastName: lastName || "",
+//       username: username || "",
+//       password: password || ""
+//     };
+//   },
+
+
+//   validationSchema: Yup.object().shape({
+//     firstName: Yup.string().required("Please enter your first name"),
+//     lastName: Yup.string().required("Please enter your last name"),
+//     username: Yup.string().required("Please enter a username"),
+//     password: Yup.string().required("Please enter a password")
+//   }),
+//   handleSubmit(values, { setStatus, resetForm }) {
+//     axios
+//       .post("https://wunderlist-2.herokuapp.com/api/auth/register", values)
+//       .then(res => {
+//         console.log(res);
+//         resetForm();
+//       });
+//   }
+// })(SignIn);
+
+const formikFormSignIn = withFormik({
+  mapPropsToValues({ username, password }) {
     return {
-      firstName: firstName || "",
-      lastName: lastName || "",
       username: username || "",
       password: password || ""
     };
   },
-
-
   validationSchema: Yup.object().shape({
-    firstName: Yup.string().required("Please enter your first name"),
-    lastName: Yup.string().required("Please enter your last name"),
-    username: Yup.string().required("Please enter a username"),
-    password: Yup.string().required("Please enter a password")
+    username: Yup.string().required("Enter your username"),
+    password: Yup.string().required("Enter your password")
   }),
   handleSubmit(values, { setStatus, resetForm }) {
     axios
       .post("https://wunderlist-2.herokuapp.com/api/auth/register", values)
       .then(res => {
-        console.log(res);
+        setStatus(res.data);
         resetForm();
-      });
+      })
+      .catch(err => console.error(err));
   }
-})(SignIn);
-export default SignInFormikForm;
+})(Login);
+
+
+
+export default formikFormSignIn;
